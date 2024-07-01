80967cd38c75b36a258c420a9c0694306004d4fa
cxnajder <259448@student.pwr.edu.pl>
2024-07-02 00:34:52 +0200

    Update scripts to automaticaly update submodules

--- Build.bat
+++ Build.bat
@@ -1,3 +1,4 @@
 @echo on
+git submodule update --init --recursive
 cmake -D GLFW_BUILD_DOCS=OFF -S Source -B Build
 ninja -C Build
--- Build.sh
+++ Build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+git submodule update --init --recursive
 cmake -D GLFW_BUILD_DOCS=OFF -S Source -B Build
 make -C Build
