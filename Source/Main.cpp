6fcf5add358b42962239d6f619807ea361e0d442
cxnajder <259448@student.pwr.edu.pl>
2024-07-01 23:40:41 +0200

    Add code for first simple glfw program

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,7 +1,27 @@
 #include "GLFW/glfw3.h"
 
-int main(int argc, char ** argv)
+#include <cstdlib>
+
+int main(int argc, char * argv[])
 {
-    
-    return 0;
+    GLFWwindow * window;
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    window = glfwCreateWindow(640, 480, "First GLFW Program", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
