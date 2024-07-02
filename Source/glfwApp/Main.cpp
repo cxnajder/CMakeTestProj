3c223688e675ea38cf600b5f99c7fbcc82e971dd
cxnajder <259448@student.pwr.edu.pl>
2024-07-02 18:28:30 +0200

    Add version number in window title

--- Source/glfwApp/Main.cpp
+++ Source/glfwApp/Main.cpp
@@ -3,15 +3,23 @@
 #include "GLFW/glfw3.h"
 
 #include <cstdlib>
+#include <string>
+#include <sstream>
 
 int main(int argc, char * argv[])
 {
     GLFWwindow * window;
+    std::string windowTitle;
+    {
+        std::stringstream ss;
+        ss << "First GLFW Program" << " v" << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR;
+        windowTitle = ss.str();\
+    }
 
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
-    window = glfwCreateWindow(640, 480, "First GLFW Program", NULL, NULL);
+    window = glfwCreateWindow(640, 480, windowTitle.c_str(), NULL, NULL);
     if (!window)
     {
         glfwTerminate();
