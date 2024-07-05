5b85f7a9a6e2b2f961ed3cb413623bcea99d9568
cxnajder <98214055+cxnajder@users.noreply.github.com>
2024-07-05 15:39:16 +0200

    Add default options flags to shell script

--- Build.sh
+++ Build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git submodule update --init --recursive
-cmake -S Source -B Build -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_WAYLAND=OFF -DGLFW_BUILD_X11=OFF
+cmake -S Source -B Build -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_WAYLAND=OFF -DGLFW_BUILD_X11=OFF -DBUILD_GLFW_APP=ON -DBUILD_GTEST_APP=ON
 make -C Build
