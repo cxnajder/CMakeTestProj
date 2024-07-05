dc8e2964d264d970f52eb6be1472fe98331f3dea
cxnajder <98214055+cxnajder@users.noreply.github.com>
2024-07-05 15:40:15 +0200

    Add default options flags to batch script

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
 @echo on
 git submodule update --init --recursive
-cmake -S Source -B Build -DGLFW_BUILD_DOCS=OFF
+cmake -S Source -B Build -DGLFW_BUILD_DOCS=OFF -DBUILD_GLFW_APP=ON -DBUILD_GTEST_APP=ON
 ninja -C Build
